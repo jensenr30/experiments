871af03cd407e8b0743b2cb762e1984451f7eb02
Ryan Jensen <jensenr30@gmail.com>
2023-02-14 21:58:57 -0600

    Adding thread_test directory

---
+++ thread_test/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <Windows.h>
+
+// trying to calculate prime numbers with threads
+
+int primes[1000];
+int i, j;
+
+void *threadFunc(void *arg)
+{
+	j = 4;
+	int i;
+	
+	
+	
+	
+	
+	
+	return NULL;
+}
+
+
+
+int main()
+{
+	
+	pthread_t pth; // this is our identifier
+	
+	
+	primes[0] = 2;
+	primes[1] = 3;
+	primes[2] = 5;
+	int i = 3;
+	
+	pthread_create( &pth, NULL, threadFunc, NULL);
+	int k;
+	for(i = 3; i < 1000; i += 2)
+	{
+		while( i >= j*2 ) wait;
+		
+		for(k = 0; a[i] >= k * 2; k++)
+		{
+			
+		
+		
+	}
+	
+	
+	return 0;
+}
