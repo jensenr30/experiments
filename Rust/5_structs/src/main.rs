a54aaf8d67131e228d567eb495cd58cb5b6928c1
Ryan Jensen <jensenr30@gmail.com>
2023-02-15 20:23:09 -0600

    learning Rust - structs

--- Rust/5_structs/src/main.rs
+++ Rust/5_structs/src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     print_user(&u);
     // create a new structure with some new data, the rest of the data gets copied from the old struct.
     let u2 = User {
-        username: String::from("mkultra"),
-        email: String::from("mkultra@cia.gov"),
+        username: String::from("bob"),
+        email: String::from("bob@cia.gov"),
         ..u
     };
     print_user(&u2);
@@ -19,22 +19,32 @@ fn main() {
     println!("Origin = ({}, {}, {}).",origin.0, origin.1, origin.2);
 
     // create a rectangle struct
-    let scale:u32 = 2;
-    let rect = Rectangle{
-        width: dbg!(20*scale),
-        height: dbg!(50*scale),
+    let rect1 = Rectangle {
+        width: 20,
+        height: 50,
     };
-    println!("rectangle is {:#?}",rect);
-    println!(
-        "The area of rectangle is {} square units.",
-        area(&rect)
-    );
-    dbg!(&rect);
+    let rect2 = Rectangle {
+        width: 10,
+        height: 60,
+    };
+    let rect3 = Rectangle {
+        width: 15,
+        height: 40,
+    };
+    let rect4 = Rectangle::square(20);
+    dbg!(&rect1);
+    dbg!(&rect2);
+    dbg!(&rect3);
+    dbg!(&rect4);
+    println!("The area of rectangle is {} square units.", rect1.area());
+    println!("Question: will rect2 fit in rect1? Answer: {}",rect1.can_hold_yes_no(&rect2));
+    println!("Question: will rect3 fit in rect1? Answer: {}",rect1.can_hold_yes_no(&rect3));
+    println!("Question: will rect4 fit in rect1? Answer: {}",rect1.can_hold_yes_no(&rect4));
 }
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.width * rect.height
-}
+// fn area(rect: &Rectangle) -> u32 {
+//     rect.width * rect.height
+// }
 
 struct User {
     active: bool,
@@ -67,4 +77,22 @@ struct Point(f64, f64, f64);
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        (self.width >= other.width) && (self.height >= other.height)
+    }
+    fn can_hold_yes_no(&self, other: &Rectangle) -> &str {
+        if self.can_hold(other) {return "yes"} else {return "no"}
+    }
 }
