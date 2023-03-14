081c54150ebcc8dc094fe93ad29e20c77536d620
Ryan Jensen <jensenr30@gmail.com>
2023-03-14 02:11:05 -0500

    learning Rust

--- Rust/8_collections/src/main.rs
+++ Rust/8_collections/src/main.rs
@@ -51,5 +51,58 @@ fn main() {
     for i in 0..4 {
         s.push_str(slice);
     }
-    println!("{}",s);
+    println!("s is {s}");
+
+    let data = "string literal";
+    let s = data.to_string();
+    println!("String created from {s}");
+    let s = "string literal can be directly converted to String.".to_string();
+    println!("{s}");
+    let mut s = String::from("Another method to create a String from literal");
+    println!("{s}");
+    s.push_str(" lets push some characters onto that string!");
+    println!("{s}");
+    // lets push a String onto itself.  We need to clone() the String, because
+    // otherwise we would be passing both a mutable and immutable reference of
+    // the same String to the push_str function which is illegal.
+    s.push_str(&s.clone());
+    println!("{s}");
+    // concatenation with the + operator
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
+    println!("{s3}");
+    let sl1 = "string literal 1. ";
+    let sl2 = "string literal 2.";
+    let s3 = sl1.to_string() + sl2;
+    println!("{s3}");
+
+    // Storing Keys with Associated Values in Hash Maps
+    use std::collections::HashMap;
+    let mut scores: HashMap<String, i64> = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    println!("{team_name} scored {score} points.");
+    // iterate over every key, value pair
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    // Adding a Key and Value Only if a Key Isn't Present
+    let mut scores2 = HashMap::new();
+    scores2.insert(String::from("Blue"), 20);
+    scores2.entry(String::from("Yellow")).or_insert(60);
+    scores2.entry(String::from("Blue")).or_insert(60);
+    println!("{:?}", scores2);
+
+    // Updating a value Based on the Old Value
+    let text = "Hello world wonderful world";
+    let mut word_hist = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = word_hist.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", word_hist);
 }
