4087082a1bc3d1d2bf5bd22b51e9243a3a505c29
Ryan Jensen <jensenr30@gmail.com>
2023-03-31 00:40:06 -0500

    started on Rust chapter 12

---
+++ Rust/12_minigrep/src/main.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file!");
+
+    println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments!");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        println!("Searching for pattern: '{}'", query);
+        println!("In File: '{}'", file_path);
+        Config { query, file_path }
+    }
+}
