bf9772c7feeb966c139816d20157aaf74318f5b1
Ryan Jensen <jensenr30@gmail.com>
2023-03-31 02:38:26 -0500

    finished section 12.3

--- Rust/12_minigrep/src/main.rs
+++ Rust/12_minigrep/src/main.rs
@@ -1,29 +1,15 @@
 use std::env;
-use std::fs;
+use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file!");
-
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            panic!("not enough arguments!");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        println!("Searching for pattern: '{}'", query);
-        println!("In File: '{}'", file_path);
-        Config { query, file_path }
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
